8747d5929fc4046f388e36d3f46e4909ad3b892c
Tima02121991 <mirzakaev_timur@mail.ru>
2022-04-08 22:55:54 +0500

    new branch junit4 and read test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,41 +1,42 @@
 package ru.netology.service;
 
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
 
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn0IfAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn999IfAmountIs1001() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1001);
         int expected = 999;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn1IfAmountIs999() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(999);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+
     }
 }
